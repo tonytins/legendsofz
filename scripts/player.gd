07740a9190d2d35cb9706e46da7aee0f45c15f92
Anthony Foxclaw <noreply@tonytins.xyz>
2020-06-14 05:14:19 -0400

    Player animation tree
    - Increased friction to 500

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 const ACCELERATION = 500
 const MAX_SPEED = 80
-const FRICTION = 120
+const FRICTION = 500
 
 var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
